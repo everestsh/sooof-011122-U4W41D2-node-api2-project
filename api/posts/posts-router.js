0942479a8f767e0767c19ba35607769c1de4fb47
sooof <sooof@me.com>
2022-02-07 15:41:09 -0800

    get /api/posts && get /api/posts/:id work

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,61 @@
 // implement your posts router here
+const express = require('express')
+const router = express.Router()
+const Post = require('./posts-model')
+
+router.get('/', async (req, res)=>{
+    // res.json({message: 'TEST: get /api/posts'})
+    Post.find()
+        .then( found => {
+            // throw new Error("ouch")
+            res.status(200).json(found)
+        })
+        .catch( err=>{
+            res.status(500).json({
+                message: "The posts information could not be retrieved",
+                err: err.message,
+                stack: err.stack
+            })
+        })
+})
+
+router.get('/:id', async (req, res)=>{
+    // res.json({message: 'TEST: get /api/posts/:id '})
+    try{
+        // throw new Error("pathetic")
+        // const post = await Post.findById(100000)
+        // console.log(post)
+        
+        const post = await Post.findById(req.params.id)
+        if(!post){
+            res.status(404).json({message: 'The post with the specified ID does not exist' })
+        }else{
+            res.status(200).json(post)
+        }
+       
+    }catch(err){
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            err: err.message,
+            stack: err.stack
+        })
+    }
+})
+
+router.post('/', async (req, res)=>{
+    res.json({message: 'TEST: post /api/posts  '})
+})
+
+router.delete('/:id', async (req, res)=>{
+    res.json({message: 'TEST: delete /api/posts/:id '})
+})
+
+router.put('/:id', async (req, res)=>{
+    res.json({message: 'TEST: put /api/posts/:id '})
+})
+
+router.get('/:id/messages', async (req, res)=>{
+    res.json({message: 'TEST: get /api/posts/:id/messages '})
+})
+
+module.exports = router
